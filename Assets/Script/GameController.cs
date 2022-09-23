40d9ef489c7fcefe5a416bd7e1fbe59f4a5f9651
guppi65513461 <origuchi.takeshi.561@s.kyushu-u.ac.jp>
2022-09-24 00:14:31 +0900

    アンサーカードの実装途中

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
-
+using System.Linq;
 
 public class GameController : MonoBehaviour
 {
@@ -14,12 +14,12 @@ public class GameController : MonoBehaviour
 
     public Kanji_Data Kanji_Data;
 
-    public List<Kanji_Data.Param> param = new List<Kanji_Data.Param> ();
-    public List<Kanji_Data.Param> SectionKanjis = new List<Kanji_Data.Param> ();
-    
+    public List<Kanji_Data.Param> param = new List<Kanji_Data.Param>();
+    public List<Kanji_Data.Param> SectionKanjis = new List<Kanji_Data.Param>();
+
     //1~200, 201~400, 401~585, 586~766 でそれぞれ3,4,5,6年の漢字が相当。
 
-    [SerializeField, Range(0,39)]
+    [SerializeField, Range(0, 39)]
     public int section_id; //セクションの番号
 
     List<int> question_numbers; //該当する漢字のIDリスト
@@ -30,7 +30,6 @@ public class GameController : MonoBehaviour
     //基本的にセクションは20ごとに区切られる。
 
 
-
     public GameObject ANSWER_ZONE; //回答用の処理を司る部分。レベルに応じて色々変える予定。
     List<GameObject> AnswerCards12; //回答カードのリスト。1と2で用いる。
 
@@ -38,18 +37,26 @@ public class GameController : MonoBehaviour
     void Start()
     {
         section_id = StageButtonController.CHOSEN_STAGE_ID; //選んだセクションIDを代入
-
         kanji_start_id = (section_id) * 20;
         kanji_end_id = (kanji_start_id) + 20;
-        
+
         kanji_question_now = 0;
 
         param = Kanji_Data.param; //全漢字のデータを取得
-        SectionKanjis = param.GetRange(kanji_start_id,kanji_end_id); //セクションの出題範囲の漢字に絞り込む
+        SectionKanjis = param.GetRange(kanji_start_id, kanji_end_id); //セクションの出題範囲の漢字に絞り込む
 
         //AnswerCards12 = ANSWER_ZONE.GetComponentsInChildren<GameObject>();
 
-
+        var int_choice = 0;
+        foreach (Transform childTransform in ANSWER_ZONE.transform)
+        {
+            //Debug.Log(childTransform);
+            var ans = childTransform.GetComponentInChildren<TextMeshProUGUI>();
+            //var kanji_choices = SectionKanjis[kanji_question_now].choices.Split(",");
+            ans.text = SectionKanjis[kanji_question_now].choices[int_choice*2].ToString(); //string型から選択肢部分のみを抽出、代入
+            Debug.Log(ans.text);
+            int_choice += 1;
+        }
 
     }
 
@@ -62,21 +69,62 @@ public class GameController : MonoBehaviour
 
 
 
-    public void testNextQuestion(){ 
+    public void testNextQuestion()
+    {
         //次の問題を出題、あるいはクリア処理
-        if (kanji_question_now < 19) {
+        if (kanji_question_now < 19)
+        {
             kanji_question_now += 1;
-        }else{
+        }
+        else
+        {
+            SceneManager.LoadScene("ClearScene");
+        }
+    }
+
+    public void NextQuestion()
+    {
+        //次の問題を出題、あるいはクリア処理   
+        if (kanji_question_now == 0)
+        {
+            var choice_id = 0;
+            foreach (Transform childTransform in ANSWER_ZONE.transform)
+            {
+                Debug.Log(childTransform);
+                var ans = childTransform.GetComponentInChildren<TextMeshProUGUI>();
+                ans.text = SectionKanjis[kanji_question_now].choices[choice_id].ToString();
+                Debug.Log(ans.text);
+                choice_id += 1;
+            }
+            choice_id = 0;
+        }
+        else if (kanji_question_now < 20)
+        {
+            kanji_question_now += 1;
+            var choice_id = 0;
+            foreach (Transform childTransform in ANSWER_ZONE.transform)
+            {
+                Debug.Log(childTransform);
+                var ans = childTransform.GetComponentInChildren<TextMeshProUGUI>();
+                ans.text = SectionKanjis[kanji_question_now].choices[choice_id].ToString();
+                Debug.Log(ans.text);
+                choice_id += 1;
+            }
+            choice_id = 0;
+        }
+        else
+        {
             SceneManager.LoadScene("ClearScene");
         }
         //Debug.Log(kanji_question_now);
-        
+
     }
 
 
 
 
-    public void toMainScene(){
+    public void toMainScene()
+    {
         SceneManager.LoadScene("MainScene");
     }
 
